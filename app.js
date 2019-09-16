74c593e0ca52c05d9836be1b747289beffcd3fea
Stephen McMillen <Shtevetm@gmail.com>
2019-09-16 14:07:26 -0500

    added event listener to search button

--- app.js
+++ app.js
@@ -7,27 +7,30 @@ var startYear = "1980";
 var endYear = "1990";
 var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userInput + "&api-key=" + apiKey;
 
-if (filter.length > 0) {
-    queryUrl += "&fq=" + filter;
-}
-
-if (startYear.length > 0) {
-    queryUrl += "&begin_date=" + startYear + "0101";
-}
-if (endYear.length > 0) {
-    queryUrl += "&end_date=" + endYear + "1231";
-}
-
-// if (pages > 0) {
-//     queryUrl += "&pages=" + pages;
-// }
-
-$.ajax({
-    url: queryUrl,
-    method: "GET"
+$("search").on("click", function(){
+    if (filter.length > 0) {
+        queryUrl += "&fq=" + filter;
+    }
+    
+    if (startYear.length > 0) {
+        queryUrl += "&begin_date=" + startYear + "0101";
+    }
+    if (endYear.length > 0) {
+        queryUrl += "&end_date=" + endYear + "1231";
+    }
+    
+    // if (pages > 0) {
+    //     queryUrl += "&pages=" + pages;
+    // }
+    
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+    
+    .then(function (response){
+        console.log(response)
+    
+    });
+    
 })
-
-.then(function (response){
-    console.log(response)
-
-});
