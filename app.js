ffcf7201c1d6aafe1bfd25537f1fb775767be00f
Chris Mlinac <chrismlinac@gmail.com>
2019-09-16 13:42:23 -0500

    add start year filter, start pagination

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var apiKey = "oXYug8o8rYFHQV2yGSEEqpjq6rOGoz17";
 var userInput = "news";
 var filter = "";
+var numResults = 10;
+var pages = numResults / 10;
+var startYear = "1990";
+var endYear = "";
 var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userInput + "&api-key=" + apiKey;
 
 if (filter.length > 0) {
     queryUrl += "&fq=" + filter;
 }
+
+if (startYear.length > 0) {
+    queryUrl += "&begin_date" + startYear + "0101";
+}
+
+// if (pages > 0) {
+//     queryUrl += "&pages=" + pages;
+// }
 
 $.ajax({
     url: queryUrl,
