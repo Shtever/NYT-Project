4f64e31c214e86cf5c19c024b07d4908ed8e6b8d
Bailey Beck <baileymbeck@gmail.com>
2019-09-16 13:51:50 -0500

    fixed start date, added end date

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var userInput = "news";
 var filter = "";
 var numResults = 10;
 var pages = numResults / 10;
-var startYear = "1990";
-var endYear = "";
+var startYear = "1980";
+var endYear = "1990";
 var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userInput + "&api-key=" + apiKey;
 
 if (filter.length > 0) {
@@ -12,7 +12,10 @@ if (filter.length > 0) {
 }
 
 if (startYear.length > 0) {
-    queryUrl += "&begin_date" + startYear + "0101";
+    queryUrl += "&begin_date=" + startYear + "0101";
+}
+if (endYear.length > 0) {
+    queryUrl += "&end_date=" + endYear + "1231";
 }
 
 // if (pages > 0) {
