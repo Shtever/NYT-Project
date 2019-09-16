9b586fbb8c65c6c1fb532fd2ac02d61065886b72
Chris Mlinac <chrismlinac@gmail.com>
2019-09-16 13:24:57 -0500

    add optional basic filter

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var apiKey = "oXYug8o8rYFHQV2yGSEEqpjq6rOGoz17";
 var userInput = "news";
+var filter = "";
 var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userInput + "&api-key=" + apiKey;
+
+if (filter.length > 0) {
+    queryUrl += "&fq=" + filter;
+}
 
 $.ajax({
     url: queryUrl,
@@ -8,6 +13,6 @@ $.ajax({
 })
 
 .then(function (response){
-console.log(response)
+    console.log(response)
 
 });
