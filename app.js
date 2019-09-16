62ae3a79cec0a87a2c06497311ec00639b89151f
Chris Mlinac <chrismlinac@gmail.com>
2019-09-16 14:19:32 -0500

    add query string, try start/end year

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 var apiKey = "oXYug8o8rYFHQV2yGSEEqpjq6rOGoz17";
-var userInput = "news";
-var filter = "";
-var numResults = 10;
-var pages = numResults / 10;
-var startYear = "1980";
-var endYear = "1990";
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userInput + "&api-key=" + apiKey;
 
-$("search").on("click", function(){
+
+$("#search").on("click", function(){
+
+    var userInput = $("#searchTerm").text();
+    var filter = "";
+    var numResults = 10;
+    var pages = numResults / 10;
+    var startYear = $("#startYear").text();
+    var endYear = $("#endYear").text();
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userInput + "&api-key=" + apiKey;
+
+    console.log(queryUrl);
+
     if (filter.length > 0) {
         queryUrl += "&fq=" + filter;
     }
-    
+    console.log(startYear);
+    console.log(startYear.length);
     if (startYear.length > 0) {
         queryUrl += "&begin_date=" + startYear + "0101";
     }
+
     if (endYear.length > 0) {
         queryUrl += "&end_date=" + endYear + "1231";
     }
@@ -33,4 +40,8 @@ $("search").on("click", function(){
     
     });
     
-})
+});
+
+$("#clear").on("click", function() {
+    $("#results").empty();
+});
